1a95dcf423328c82e7970069d484a5c591a09f86
Johan T. Halseth <johanth@gmail.com>
2018-10-04 14:50:12 +0100

    lnrpc/autopilot: add SetNodeScores RPC

--- lnrpc/autopilot/autopilot.pb.gw.go
+++ lnrpc/autopilot/autopilot.pb.gw.go
@@ -50,6 +50,19 @@ func request_Autopilot_Enable_0(ctx context.Context, marshaler runtime.Marshaler
 
 }
 
+func request_Autopilot_SetNodeScores_0(ctx context.Context, marshaler runtime.Marshaler, client AutopilotClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq SetNodeScoresRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.SetNodeScores(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterAutopilotHandlerFromEndpoint is same as RegisterAutopilotHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterAutopilotHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -138,6 +151,35 @@ func RegisterAutopilotHandler(ctx context.Context, mux *runtime.ServeMux, conn *
 
 	})
 
+	mux.Handle("POST", pattern_Autopilot_SetNodeScores_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Autopilot_SetNodeScores_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Autopilot_SetNodeScores_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -145,10 +187,14 @@ var (
 	pattern_Autopilot_GetStatus_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "status"}, ""))
 
 	pattern_Autopilot_Enable_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "enable"}, ""))
+
+	pattern_Autopilot_SetNodeScores_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "nodescores"}, ""))
 )
 
 var (
 	forward_Autopilot_GetStatus_0 = runtime.ForwardResponseMessage
 
 	forward_Autopilot_Enable_0 = runtime.ForwardResponseMessage
+
+	forward_Autopilot_SetNodeScores_0 = runtime.ForwardResponseMessage
 )

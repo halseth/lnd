645cbf85e07780d8d235697b0b591e516acc68a4
Johan T. Halseth <johanth@gmail.com>
2018-10-04 14:50:11 +0100

    lncli: add experimental command autopilot-enable

--- cmd/lncli/experimental.go
+++ cmd/lncli/experimental.go
@@ -43,10 +43,49 @@ func autopilotStatus(ctx *cli.Context) error {
 	return nil
 }
 
+var autopilotEnableCommand = cli.Command{
+	Name:        "autopilot-enable",
+	Category:    "Autopilot",
+	Usage:       "",
+	Description: "",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "disable",
+			Usage: "",
+		},
+	},
+	Action: actionDecorator(autopilotEnable),
+}
+
+func autopilotEnable(ctx *cli.Context) error {
+	ctxb := context.Background()
+	client, cleanUp := getAutopilotClient(ctx)
+	defer cleanUp()
+
+	// By default we will try to enable the autopilot.
+	req := &autopilot.EnableRequest{
+		Enable: true,
+	}
+
+	// If the --disable flag is set, then disable autopilot instead.
+	if ctx.IsSet("disable") {
+		req.Enable = false
+	}
+
+	resp, err := client.Enable(ctxb, req)
+	if err != nil {
+		return err
+	}
+
+	printRespJSON(resp)
+	return nil
+}
+
 // extraCommands will return the set of commands to enable for experimental
 // builds.
 func extraCommands() []cli.Command {
 	return []cli.Command{
 		autopilotStatusCommand,
+		autopilotEnableCommand,
 	}
 }

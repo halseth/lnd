6f0e7de51a7612b386b46dc55cb9c7dc68371179
Valentine Wallace <vwallace@protonmail.com>
2019-01-12 18:59:43 +0100

    multi: partition lnwire.ChanUpdateFlag into ChannelFlags and MessageFlags
    In this commit:
    
    * we partition lnwire.ChanUpdateFlag into two (ChanUpdateChanFlags and
    ChanUpdateMsgFlags), from a uint16 to a pair of uint8's
    
    * we rename the ChannelUpdate.Flags to ChannelFlags and add an
    additional MessageFlags field, which will be used to indicate the
    presence of the optional field HtlcMaximumMsat within the ChannelUpdate.
    
    * we partition ChannelEdgePolicy.Flags into message and channel flags.
    This change corresponds to the partitioning of the ChannelUpdate's Flags
    field into MessageFlags and ChannelFlags.
    
    Co-authored-by: Johan T. Halseth <johanth@gmail.com>

--- rpcserver.go
+++ rpcserver.go
@@ -3698,7 +3698,7 @@ func marshalDbEdge(edgeInfo *channeldb.ChannelEdgeInfo,
 			MinHtlc:          int64(c1.MinHTLC),
 			FeeBaseMsat:      int64(c1.FeeBaseMSat),
 			FeeRateMilliMsat: int64(c1.FeeProportionalMillionths),
-			Disabled:         c1.Flags&lnwire.ChanUpdateDisabled != 0,
+			Disabled:         c1.ChannelFlags&lnwire.ChanUpdateDisabled != 0,
 		}
 	}
 
@@ -3708,7 +3708,7 @@ func marshalDbEdge(edgeInfo *channeldb.ChannelEdgeInfo,
 			MinHtlc:          int64(c2.MinHTLC),
 			FeeBaseMsat:      int64(c2.FeeBaseMSat),
 			FeeRateMilliMsat: int64(c2.FeeProportionalMillionths),
-			Disabled:         c2.Flags&lnwire.ChanUpdateDisabled != 0,
+			Disabled:         c2.ChannelFlags&lnwire.ChanUpdateDisabled != 0,
 		}
 	}
 

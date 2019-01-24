f6470cb489240acb478cd042c4f257eda00a4073
Johan T. Halseth <johanth@gmail.com>
2019-01-24 13:22:31 +0100

    switch+router+server: move NextPaymentID to router
    This commit moves the responsibility of generating a unique payment ID
    from the switch to the router. This will make it easier for the router
    to keep track of which HTLCs were successfully forwarded onto the
    network, as it can replay the same HTLCs as long as the paymentIDs are
    kept.

--- htlcswitch/switch.go
+++ htlcswitch/switch.go
@@ -208,8 +208,6 @@ type Switch struct {
 	pendingPayments map[uint64]*pendingPayment
 	pendingMutex    sync.RWMutex
 
-	paymentSequencer Sequencer
-
 	// control provides verification of sending htlc mesages
 	control ControlTower
 
@@ -288,16 +286,10 @@ func New(cfg Config, currentHeight uint32) (*Switch, error) {
 		return nil, err
 	}
 
-	sequencer, err := NewPersistentSequencer(cfg.DB)
-	if err != nil {
-		return nil, err
-	}
-
 	return &Switch{
 		bestHeight:        currentHeight,
 		cfg:               &cfg,
 		circuits:          circuitMap,
-		paymentSequencer:  sequencer,
 		control:           NewPaymentControl(false, cfg.DB),
 		linkIndex:         make(map[lnwire.ChannelID]ChannelLink),
 		mailOrchestrator:  newMailOrchestrator(),
@@ -350,7 +342,7 @@ func (s *Switch) ProcessContractResolution(msg contractcourt.ResolutionMsg) erro
 
 // SendHTLC is used by other subsystems which aren't belong to htlc switch
 // package in order to send the htlc update.
-func (s *Switch) SendHTLC(firstHop lnwire.ShortChannelID,
+func (s *Switch) SendHTLC(firstHop lnwire.ShortChannelID, paymentID uint64,
 	htlc *lnwire.UpdateAddHTLC,
 	deobfuscator ErrorDecrypter) ([sha256.Size]byte, error) {
 
@@ -369,11 +361,6 @@ func (s *Switch) SendHTLC(firstHop lnwire.ShortChannelID,
 		paymentHash:  htlc.PaymentHash,
 		amount:       htlc.Amount,
 		deobfuscator: deobfuscator,
-	}
-
-	paymentID, err := s.paymentSequencer.NextID()
-	if err != nil {
-		return zeroPreimage, err
 	}
 
 	s.pendingMutex.Lock()
@@ -402,6 +389,7 @@ func (s *Switch) SendHTLC(firstHop lnwire.ShortChannelID,
 	// Returns channels so that other subsystem might wait/skip the
 	// waiting of handling of payment.
 	var preimage [sha256.Size]byte
+	var err error
 
 	select {
 	case e := <-payment.err:

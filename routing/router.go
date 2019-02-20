b4d9dfa4651e99c7e9aa03a7b7b5e27cd062f4be
Johan T. Halseth <johanth@gmail.com>
2019-02-20 15:19:14 +0100

    routing: move LighntingPayment to new file + serialization
    We move the LightningPayment struct to its own file, and add methods to
    serialize and deserialze the struct.

--- routing/router.go
+++ routing/router.go
@@ -1526,54 +1526,6 @@ func generateSphinxPacket(route *Route, paymentHash []byte) ([]byte,
 	}, nil
 }
 
-// LightningPayment describes a payment to be sent through the network to the
-// final destination.
-type LightningPayment struct {
-	// Target is the node in which the payment should be routed towards.
-	Target *btcec.PublicKey
-
-	// Amount is the value of the payment to send through the network in
-	// milli-satoshis.
-	Amount lnwire.MilliSatoshi
-
-	// FeeLimit is the maximum fee in millisatoshis that the payment should
-	// accept when sending it through the network. The payment will fail
-	// if there isn't a route with lower fees than this limit.
-	FeeLimit lnwire.MilliSatoshi
-
-	// PaymentHash is the r-hash value to use within the HTLC extended to
-	// the first hop.
-	PaymentHash [32]byte
-
-	// FinalCLTVDelta is the CTLV expiry delta to use for the _final_ hop
-	// in the route. This means that the final hop will have a CLTV delta
-	// of at least: currentHeight + FinalCLTVDelta. If this value is zero,
-	// then a default value of DefaultFinalCLTVDelta will be used.
-	FinalCLTVDelta uint16
-
-	// PayAttemptTimeout is a timeout value that we'll use to determine
-	// when we should should abandon the payment attempt after consecutive
-	// payment failure. This prevents us from attempting to send a payment
-	// indefinitely.
-	PayAttemptTimeout time.Duration
-
-	// RouteHints represents the different routing hints that can be used to
-	// assist a payment in reaching its destination successfully. These
-	// hints will act as intermediate hops along the route.
-	//
-	// NOTE: This is optional unless required by the payment. When providing
-	// multiple routes, ensure the hop hints within each route are chained
-	// together and sorted in forward order in order to reach the
-	// destination successfully.
-	RouteHints [][]HopHint
-
-	// OutgoingChannelID is the channel that needs to be taken to the first
-	// hop. If zero, any channel may be used.
-	OutgoingChannelID uint64
-
-	// TODO(roasbeef): add e2e message?
-}
-
 // SendPayment attempts to send a payment as described within the passed
 // LightningPayment. This function is blocking and will return either: when the
 // payment is successful, or all candidates routes have been attempted and

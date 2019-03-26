abae672278aa01e74e3061d410f785a61005fd5d
Johan T. Halseth <johanth@gmail.com>
2019-03-26 09:10:34 +0100

    channeldb/payments+control_tower: split OutgoingPayments
    This commit changes the format used to store OutgoingPayments within the
    DB. Previously this was serialized as one continuous struct, which also
    contained an Invoice struct we where only using a few fields of. We now
    split it up into two simpler sub-structs CreationInfo and SettleInfo.
    
    We also want to associate OutgoingPayments more closely with payment
    statuses, so we move to this hierarchy:
    
    There's one top-level bucket "outgoingPaymentBucket" which contains a
    set of sub-buckets indexed by a payment's payment hash. Each such
    sub-bucket contains 3 fields:
    paymentStatusKey -> the payment's status
    paymentCreationInfoKey -> the payment's CreationInfo.
    paymentSettleInfoKey -> the payment's SettleInfo.
    
    The CreationInfo is information that is static during the whole payment
    lifcycle. SettleInfo is information only know when a payment succeeds.
    It therefore makes sense to split them.
    
    We keep legacy serialization code for migration puproses.

--- channeldb/payments.go
+++ channeldb/payments.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
+	"time"
 
 	"github.com/coreos/bbolt"
 	"github.com/lightningnetwork/lnd/lnwire"
@@ -21,6 +23,14 @@ var (
 	// paymentStatusKey is a key used in the payment's sub-bucket to store
 	// the status of the payment.
 	paymentStatusKey = []byte("payment-status")
+
+	// paymentCreationInfoKey is a key used in the payment's sub-bucket to store
+	// the creation info of the payment.
+	paymentCreationInfoKey = []byte("payment-creation-info")
+
+	// paymentSettleInfoKey is a key used in the payment's sub-bucket to store
+	// the settle info of the payment.
+	paymentSettleInfoKey = []byte("payment-settle-info")
 
 	// paymentBucket is the name of the bucket within the database that
 	// stores all data related to payments.
@@ -315,4 +325,247 @@ func deserializeOutgoingPayment(r io.Reader) (*OutgoingPayment, error) {
 	}
 
 	return p, nil
+}
+
+// CreationInfo is the information necessary to have ready when initiating a
+// payment, moving it into state InFlight.
+type CreationInfo struct {
+	// PaymentHash is the hash this payment is paying to.
+	PaymentHash [32]byte
+
+	// Value is the amount we are paying.
+	Value lnwire.MilliSatoshi
+
+	// CreatingDate is the time when this payment was initiated.
+	CreationDate time.Time
+
+	// PaymentRequest is the full payment request, if any.
+	PaymentRequest []byte
+}
+
+// SettleInfo is the information to provide to settle an in-flight payment.
+type SettleInfo struct {
+	// PaymentPreimage is the preImage of a successful payment. This serves
+	// as a proof of payment.
+	PaymentPreimage [32]byte
+
+	// Fee is the total fee paid for the payment in milli-satoshis.
+	Fee lnwire.MilliSatoshi
+
+	// TotalTimeLock is the total cumulative time-lock in the HTLC extended
+	// from the second-to-last hop to the destination.
+	TimeLockLength uint32
+
+	// Path encodes the path the payment took through the network. The path
+	// excludes the outgoing node and consists of the hex-encoded
+	// compressed public key of each of the nodes involved in the payment.
+	Path [][33]byte
+}
+
+// other name?
+type SendPayment struct {
+	// Creation info is populated when the payment is initiated, and is
+	// available for all payments.
+	CreationInfo
+
+	// SettleInfo is only available when the payment has successfully been
+	// settled.
+	SettleInfo
+}
+
+// FetchAllPayments returns all outgoing payments in DB.
+func (db *DB) FetchAllPaymentsV9() ([]*SendPayment, error) {
+	var payments []*SendPayment
+
+	err := db.View(func(tx *bbolt.Tx) error {
+		paymentsBucket := tx.Bucket(outgoingPaymentBucket)
+		if paymentsBucket == nil {
+			return ErrNoPaymentsCreated
+		}
+
+		return paymentsBucket.ForEach(func(k, v []byte) error {
+			bucket := paymentsBucket.Bucket(k)
+			if bucket == nil {
+				return fmt.Errorf("non bucket element")
+			}
+
+			p := &SendPayment{}
+
+			b := bucket.Get(paymentCreationInfoKey)
+			if b == nil {
+				return nil
+			}
+
+			r := bytes.NewReader(b)
+			c, err := deserializeCreationInfo(r)
+			if err != nil {
+				return err
+			}
+			p.CreationInfo = *c
+
+			b = bucket.Get(paymentSettleInfoKey)
+			if b != nil {
+
+				r = bytes.NewReader(b)
+				s, err := deserializeSettleInfo(r)
+				if err != nil {
+					return err
+				}
+				p.SettleInfo = *s
+			}
+
+			payments = append(payments, p)
+			return nil
+		})
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return payments, nil
+}
+
+// DeleteAllPayments deletes all payments from DB.
+func (db *DB) DeleteAllPaymentsV9() error {
+	return db.Update(func(tx *bbolt.Tx) error {
+		err := tx.DeleteBucket(outgoingPaymentBucket)
+		if err != nil && err != bbolt.ErrBucketNotFound {
+			return err
+		}
+
+		_, err = tx.CreateBucket(outgoingPaymentBucket)
+		return err
+	})
+}
+
+func serializeCreationInfo(w io.Writer, c *CreationInfo) error {
+	var scratch [8]byte
+
+	if _, err := w.Write(c.PaymentHash[:]); err != nil {
+		return err
+	}
+
+	byteOrder.PutUint64(scratch[:], uint64(c.Value))
+	if _, err := w.Write(scratch[:]); err != nil {
+		return err
+	}
+
+	byteOrder.PutUint64(scratch[:], uint64(c.CreationDate.Unix()))
+	if _, err := w.Write(scratch[:]); err != nil {
+		return err
+	}
+
+	byteOrder.PutUint32(scratch[:4], uint32(len(c.PaymentRequest)))
+	if _, err := w.Write(scratch[:4]); err != nil {
+		return err
+	}
+
+	if _, err := w.Write(c.PaymentRequest[:]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deserializeCreationInfo(r io.Reader) (*CreationInfo, error) {
+	var scratch [8]byte
+
+	c := &CreationInfo{}
+
+	if _, err := r.Read(c.PaymentHash[:]); err != nil {
+		return nil, err
+	}
+
+	if _, err := r.Read(scratch[:]); err != nil {
+		return nil, err
+	}
+	c.Value = lnwire.MilliSatoshi(byteOrder.Uint64(scratch[:]))
+
+	if _, err := r.Read(scratch[:]); err != nil {
+		return nil, err
+	}
+	c.CreationDate = time.Unix(int64(byteOrder.Uint64(scratch[:])), 0)
+
+	if _, err := r.Read(scratch[:4]); err != nil {
+		return nil, err
+	}
+
+	reqLen := uint32(byteOrder.Uint32(scratch[:4]))
+	payReq := make([]byte, reqLen)
+
+	if _, err := r.Read(payReq[:]); err != nil {
+		return nil, err
+	}
+	c.PaymentRequest = payReq
+
+	return c, nil
+}
+
+func serializeSettleInfo(w io.Writer, s *SettleInfo) error {
+	var scratch [8]byte
+
+	byteOrder.PutUint64(scratch[:], uint64(s.Fee))
+	if _, err := w.Write(scratch[:]); err != nil {
+		return err
+	}
+
+	// First write out the length of the bytes to prefix the value.
+	pathLen := uint32(len(s.Path))
+	byteOrder.PutUint32(scratch[:4], pathLen)
+	if _, err := w.Write(scratch[:4]); err != nil {
+		return err
+	}
+
+	// Then with the path written, we write out the series of public keys
+	// involved in the path.
+	for _, hop := range s.Path {
+		if _, err := w.Write(hop[:]); err != nil {
+			return err
+		}
+	}
+
+	byteOrder.PutUint32(scratch[:4], s.TimeLockLength)
+	if _, err := w.Write(scratch[:4]); err != nil {
+		return err
+	}
+
+	if _, err := w.Write(s.PaymentPreimage[:]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func deserializeSettleInfo(r io.Reader) (*SettleInfo, error) {
+	var scratch [8]byte
+	s := &SettleInfo{}
+
+	if _, err := r.Read(scratch[:]); err != nil {
+		return nil, err
+	}
+	s.Fee = lnwire.MilliSatoshi(byteOrder.Uint64(scratch[:]))
+
+	if _, err := r.Read(scratch[:4]); err != nil {
+		return nil, err
+	}
+	pathLen := byteOrder.Uint32(scratch[:4])
+
+	path := make([][33]byte, pathLen)
+	for i := uint32(0); i < pathLen; i++ {
+		if _, err := r.Read(path[i][:]); err != nil {
+			return nil, err
+		}
+	}
+	s.Path = path
+
+	if _, err := r.Read(scratch[:4]); err != nil {
+		return nil, err
+	}
+	s.TimeLockLength = byteOrder.Uint32(scratch[:4])
+
+	if _, err := r.Read(s.PaymentPreimage[:]); err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }

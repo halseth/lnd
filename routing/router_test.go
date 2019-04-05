99584f122393b3adef0093537cd065729bc4df60
Johan T. Halseth <johanth@gmail.com>
2019-04-05 17:36:13 +0200

    routing/router: add payment state machine
    This commit makes the router use the ControlTower to drive a payment
    state machine, to keep track of active payments across restarts.  This
    lets the router resume payments on startup, such that their final
    results can be handled and stored when ready.

--- routing/router_test.go
+++ routing/router_test.go
@@ -53,6 +53,7 @@ func (c *testCtx) RestartRouter() error {
 		Graph:     c.graph,
 		Chain:     c.chain,
 		ChainView: c.chainView,
+		Control:   makeMockControlTower(),
 		SendToSwitch: func(*route.Route, [32]byte, uint64) error {
 			return nil
 		},
@@ -93,6 +94,7 @@ func createTestCtxFromGraphInstance(startingHeight uint32, graphInstance *testGr
 		Graph:     graphInstance.graph,
 		Chain:     chain,
 		ChainView: chainView,
+		Control:   makeMockControlTower(),
 		SendToSwitch: func(*route.Route, [32]byte, uint64) error {
 			return nil
 		},
@@ -1699,6 +1701,7 @@ func TestWakeUpOnStaleBranch(t *testing.T) {
 		Graph:     ctx.graph,
 		Chain:     ctx.chain,
 		ChainView: ctx.chainView,
+		Control:   makeMockControlTower(),
 		SendToSwitch: func(route *route.Route, hash [32]byte, _ uint64) error {
 
 			return nil
@@ -2663,4 +2666,27 @@ func assertChannelsPruned(t *testing.T, graph *channeldb.ChannelGraph,
 				"zombie", channel.ChannelID)
 		}
 	}
+}
+
+type mockControlTower struct{}
+
+var _ channeldb.ControlTower = (*mockControlTower)(nil)
+
+func makeMockControlTower() *mockControlTower {
+	return &mockControlTower{}
+}
+
+func (m *mockControlTower) RegisterAttempt(lntypes.Hash,
+	*channeldb.CreationInfo, *channeldb.AttemptInfo) error {
+	return nil
+}
+
+func (m *mockControlTower) Success(paymentHash lntypes.Hash,
+	info *channeldb.CreationInfo, preimg lntypes.Preimage) error {
+	return nil
+}
+
+func (m *mockControlTower) Fail(paymentHash lntypes.Hash,
+	c *channeldb.CreationInfo) error {
+	return nil
 }

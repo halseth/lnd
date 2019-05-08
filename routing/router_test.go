0e52a07ca57a1c99d4690326347f6082ccd7f8dc
Johan T. Halseth <johanth@gmail.com>
2019-05-08 15:00:19 +0200

    switch+router+server: move NextPaymentID to router
    This commit moves the responsibility of generating a unique payment ID
    from the switch to the router. This will make it easier for the router
    to keep track of which HTLCs were successfully forwarded onto the
    network, as it can query the switch for existing HTLCs as long as the
    paymentIDs are kept.
    
    The router is expected to maintain a map from paymentHash->paymentID,
    such that they can be replayed on restart. This also lets the router
    check the status of a sent payment after a restart, by querying the
    switch for the paymentID in question.

--- routing/router_test.go
+++ routing/router_test.go
@@ -6,6 +6,7 @@ import (
 	"image/color"
 	"math/rand"
 	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -21,6 +22,8 @@ import (
 	"github.com/lightningnetwork/lnd/routing/route"
 	"github.com/lightningnetwork/lnd/zpay32"
 )
+
+var uniquePaymentID uint64 = 1 // to be used atomically
 
 type testCtx struct {
 	router *ChannelRouter
@@ -88,6 +91,10 @@ func createTestCtxFromGraphInstance(startingHeight uint32, graphInstance *testGr
 		GraphPruneInterval: time.Hour * 2,
 		QueryBandwidth: func(e *channeldb.ChannelEdgeInfo) lnwire.MilliSatoshi {
 			return lnwire.NewMSatFromSatoshis(e.Capacity)
+		},
+		NextPaymentID: func() (uint64, error) {
+			next := atomic.AddUint64(&uniquePaymentID, 1)
+			return next, nil
 		},
 	})
 	if err != nil {

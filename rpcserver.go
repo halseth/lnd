058a987e5ad55c0c002df23e352038e3ce1dfc0b
Johan T. Halseth <johanth@gmail.com>
2020-02-12 15:18:33 +0100

    lnrpc: add anchor_output_size to listchannels

--- rpcserver.go
+++ rpcserver.go
@@ -3068,6 +3068,7 @@ func createRPCOpenChannel(r *rpcServer, graph *channeldb.ChannelGraph,
 		LocalChanReserveSat:   int64(dbChannel.LocalChanCfg.ChanReserve),
 		RemoteChanReserveSat:  int64(dbChannel.RemoteChanCfg.ChanReserve),
 		StaticRemoteKey:       dbChannel.ChanType.IsTweakless(),
+		AnchorOutputSize:      int64(dbChannel.AnchorSize()),
 	}
 
 	for i, htlc := range localCommit.Htlcs {

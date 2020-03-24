4be5c321d41d114929fc4ae8e6ce7a5f267eed73
Johan T. Halseth <johanth@gmail.com>
2020-03-24 16:41:18 +0100

    routing/payment_session: make RequestRoute take max amt and fee limit
    In preparation for doing pathfinding for routes sending a value less
    than the total payment amount, we let the payment session take the max
    amount to send and the fee limit as arguments to RequestRoute.

--- routing/payment_session.go
+++ routing/payment_session.go
@@ -23,8 +23,11 @@ var (
 // information learned during the previous attempts.
 type PaymentSession interface {
 	// RequestRoute returns the next route to attempt for routing the
-	// specified HTLC payment to the target node.
-	RequestRoute(height uint32) (*route.Route, error)
+	// specified HTLC payment to the target node. The returned route should
+	// carry at most maxAmt to the target node, and pay at most feeLimit in
+	// fees. It can carry less if the payment is MPP.
+	RequestRoute(maxAmt, feeLimit lnwire.MilliSatoshi,
+		height uint32) (*route.Route, error)
 }
 
 // paymentSession is used during an HTLC routings session to prune the local
@@ -59,7 +62,8 @@ type paymentSession struct {
 //
 // NOTE: This function is safe for concurrent access.
 // NOTE: Part of the PaymentSession interface.
-func (p *paymentSession) RequestRoute(height uint32) (*route.Route, error) {
+func (p *paymentSession) RequestRoute(maxAmt, feeLimit lnwire.MilliSatoshi,
+	height uint32) (*route.Route, error) {
 
 	switch {
 
@@ -94,7 +98,7 @@ func (p *paymentSession) RequestRoute(height uint32) (*route.Route, error) {
 
 	restrictions := &RestrictParams{
 		ProbabilitySource: ss.MissionControl.GetProbability,
-		FeeLimit:          p.payment.FeeLimit,
+		FeeLimit:          feeLimit,
 		OutgoingChannelID: p.payment.OutgoingChannelID,
 		LastHop:           p.payment.LastHop,
 		CltvLimit:         cltvLimit,
@@ -124,7 +128,7 @@ func (p *paymentSession) RequestRoute(height uint32) (*route.Route, error) {
 		},
 		restrictions, &ss.PathFindingConfig,
 		ss.SelfNode.PubKeyBytes, p.payment.Target,
-		p.payment.Amount, finalHtlcExpiry,
+		maxAmt, finalHtlcExpiry,
 	)
 	if err != nil {
 		return nil, err
@@ -136,7 +140,7 @@ func (p *paymentSession) RequestRoute(height uint32) (*route.Route, error) {
 	route, err := newRoute(
 		sourceVertex, path, height,
 		finalHopParams{
-			amt:         p.payment.Amount,
+			amt:         maxAmt,
 			cltvDelta:   finalCltvDelta,
 			records:     p.payment.DestCustomRecords,
 			paymentAddr: p.payment.PaymentAddr,

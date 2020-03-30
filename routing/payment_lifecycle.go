21292179eb95c9a9008106b059cd03691896298d
Johan T. Halseth <johanth@gmail.com>
2020-03-30 17:02:00 +0200

    routing/payment_session: remove prebuilt payment session
    Since we no longer use payment sessions for send to route, we remove the
    prebuilt one.

--- routing/payment_lifecycle.go
+++ routing/payment_lifecycle.go
@@ -390,7 +390,7 @@ func errorToPaymentFailure(err error) channeldb.FailureReason {
 		errNoTlvPayload,
 		errNoPaymentAddr,
 		errNoPathFound,
-		errPrebuiltRouteTried:
+		errEmptyPaySession:
 
 		return channeldb.FailureReasonNoRoute
 
